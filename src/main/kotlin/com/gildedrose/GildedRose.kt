072d104c835bf9276b7fd5404ea69ec1f1ea357e
Harm De Weirdt <harm@tripled.io>
2024-03-04 17:32:47 +0100

    remove unreachable code

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -40,35 +40,29 @@ class GildedRose(var items: List<Item>) {
         if (item.quality < 50) {
             item.quality += 1
 
-          if(item.quality < 50) {
-              foo(item)
-          } else {
-              foo(item)
-          }
-            
+            if (item.quality < 50) {
+                if (item.sellIn < 11) {
+                    item.quality += 1
+                }
+                if (item.sellIn < 6) {
+                    item.quality += 1
+                }
+                item.sellIn -= 1
+                if (item.sellIn < 0) {
+                    item.quality = 0
+                }
+            } else {
+                item.sellIn -= 1
+                if (item.sellIn < 0) {
+                    item.quality = 0
+                }
+            }
+
         } else {
             item.sellIn -= 1
             if (item.sellIn < 0) {
                 item.quality = 0
             }
-        }
-    }
-
-    private fun foo(item: Item) {
-        if (item.sellIn < 11) {
-            if (item.quality < 50) {
-                item.quality += 1
-            }
-        }
-
-        if (item.sellIn < 6) {
-            if (item.quality < 50) {
-                item.quality += 1
-            }
-        }
-        item.sellIn -= 1
-        if (item.sellIn < 0) {
-            item.quality = 0
         }
     }
 
