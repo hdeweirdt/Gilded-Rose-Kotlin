93f82547ea7770d3510eb04ee759eb0bda642d77
Harm De Weirdt <harm@tripled.io>
2024-03-04 17:15:56 +0100

    remove all unreachable code

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -16,34 +16,33 @@ class GildedRose(var items: List<Item>) {
                 backStagePass(i)
             } else {
                 if (items[i].name == "Sulfuras, Hand of Ragnaros") {
-                    foo(i)
+                    sulfuras(i)
                 } else {
-                    foo(i)
-                }
-            }
-        }
-    }
-
-    private fun foo(i: Int) {
-        if (items[i].quality > 0) {
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].quality = items[i].quality - 1
-            }
-        }
-        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-            items[i].sellIn = items[i].sellIn - 1
-        }
-        if (items[i].sellIn < 0) {
-            if (items[i].name != "Aged Brie") {
-                if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                     if (items[i].quality > 0) {
                         if (items[i].name != "Sulfuras, Hand of Ragnaros") {
                             items[i].quality = items[i].quality - 1
                         }
                     }
+                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        items[i].sellIn = items[i].sellIn - 1
+                    }
+                    if (items[i].sellIn < 0) {
+                        if (items[i].name != "Aged Brie") {
+                            if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                                if (items[i].quality > 0) {
+                                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                        items[i].quality = items[i].quality - 1
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
+    }
+
+    private fun sulfuras(i: Int) {
     }
 
     private fun backStagePass(i: Int) {
