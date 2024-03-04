f33c9eed0d5ca08d8d4a3c954f26d72fc273c2f8
Harm De Weirdt <harm@tripled.io>
2024-03-04 17:10:12 +0100

    same step for backstage pass

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -10,58 +10,106 @@ class GildedRose(var items: List<Item>) {
 
     private fun doThingWithItem(i: Int) {
         if (items[i].name == "Aged Brie") {
-            if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
-            }
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-            if (items[i].sellIn < 0) {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-                }
-            }
+            agedBrie(i)
         } else {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+            if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].quality > 0) {
+                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            items[i].quality = items[i].quality - 1
+                        }
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
+
+                        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                            if (items[i].sellIn < 11) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1
+                                }
+                            }
+
+                            if (items[i].sellIn < 6) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1
+                                }
+                            }
+                        }
+                    }
+                }
+                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    items[i].sellIn = items[i].sellIn - 1
+                }
+                if (items[i].sellIn < 0) {
+                    if (items[i].name != "Aged Brie") {
+                        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                            if (items[i].quality > 0) {
+                                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                    items[i].quality = items[i].quality - 1
+                                }
+                            }
+                        } else {
+                            items[i].quality = items[i].quality - items[i].quality
+                        }
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (items[i].quality > 0) {
+                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                            items[i].quality = items[i].quality - 1
                         }
+                    }
+                } else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                            if (items[i].sellIn < 11) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1
+                                }
+                            }
+
+                            if (items[i].sellIn < 6) {
+                                if (items[i].quality < 50) {
+                                    items[i].quality = items[i].quality + 1
+                                }
                             }
                         }
                     }
                 }
-            }
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                    items[i].sellIn = items[i].sellIn - 1
+                }
+                if (items[i].sellIn < 0) {
+                    if (items[i].name != "Aged Brie") {
+                        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+                            if (items[i].quality > 0) {
+                                if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                                    items[i].quality = items[i].quality - 1
+                                }
                             }
+                        } else {
+                            items[i].quality = items[i].quality - items[i].quality
                         }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
                     }
                 }
+            }
+        }
+    }
+
+    private fun agedBrie(i: Int) {
+        if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1
+        }
+        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            items[i].sellIn = items[i].sellIn - 1
+        }
+        if (items[i].sellIn < 0) {
+            if (items[i].quality < 50) {
+                items[i].quality = items[i].quality + 1
             }
         }
     }
