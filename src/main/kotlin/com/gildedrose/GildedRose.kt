ecf2950070002329f53e3ec75ef70644d2d3a0d1
Harm De Weirdt <harm@tripled.io>
2024-03-04 17:29:04 +0100

    assignment simplification

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -23,12 +23,12 @@ class GildedRose(var items: List<Item>) {
 
     private fun regularItem(item: Item) {
         if (item.quality > 0) {
-            item.quality = item.quality - 1
+            item.quality -= 1
         }
-        item.sellIn = item.sellIn - 1
+        item.sellIn -= 1
         if (item.sellIn < 0) {
             if (item.quality > 0) {
-                item.quality = item.quality - 1
+                item.quality -= 1
             }
         }
     }
@@ -38,41 +38,41 @@ class GildedRose(var items: List<Item>) {
 
     private fun backStagePass(item: Item) {
         if (item.quality < 50) {
-            item.quality = item.quality + 1
+            item.quality += 1
 
             if (item.sellIn < 11) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
                 }
             }
 
             if (item.sellIn < 6) {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1
+                    item.quality += 1
                 }
             }
-            item.sellIn = item.sellIn - 1
+            item.sellIn -= 1
             if (item.sellIn < 0) {
-                item.quality = item.quality - item.quality
+                item.quality = 0
             }
         } else {
-            item.sellIn = item.sellIn - 1
+            item.sellIn -= 1
             if (item.sellIn < 0) {
-                item.quality = item.quality - item.quality
+                item.quality = 0
             }
         }
     }
 
     private fun agedBrie(item: Item) {
         if (item.quality < 50) {
-            item.quality = item.quality + 1
+            item.quality += 1
         }
         if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.sellIn = item.sellIn - 1
+            item.sellIn -= 1
         }
         if (item.sellIn < 0) {
             if (item.quality < 50) {
-                item.quality = item.quality + 1
+                item.quality += 1
             }
         }
     }
