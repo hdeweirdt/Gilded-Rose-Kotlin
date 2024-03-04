bfe719daa16bf63d768801d7da6136be5779c7ae
Harm De Weirdt <harm@tripled.io>
2024-03-04 17:17:01 +0100

    remove unnecessary if's

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -19,22 +19,12 @@ class GildedRose(var items: List<Item>) {
                     sulfuras(i)
                 } else {
                     if (items[i].quality > 0) {
-                        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                            items[i].quality = items[i].quality - 1
-                        }
+                        items[i].quality = items[i].quality - 1
                     }
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].sellIn = items[i].sellIn - 1
-                    }
+                    items[i].sellIn = items[i].sellIn - 1
                     if (items[i].sellIn < 0) {
-                        if (items[i].name != "Aged Brie") {
-                            if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                                if (items[i].quality > 0) {
-                                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                        items[i].quality = items[i].quality - 1
-                                    }
-                                }
-                            }
+                        if (items[i].quality > 0) {
+                            items[i].quality = items[i].quality - 1
                         }
                     }
                 }
