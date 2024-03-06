2873161466e690c8b45be582090fb460c965dcd3
Harm De Weirdt <harm@tripled.io>
2024-03-06 21:31:53 +0100

    explanatory comments + inline method

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,6 +4,10 @@ import kotlin.reflect.KFunction1
 
 class GildedRose(var items: List<Item>) {
 
+    // We are not allowed to modify the item class nor the list of items passed.
+    // We do however like having the 'tick' logic for each possible item in their appropriate class.
+    // We accomplish this by mapping the possible names to constructors for the specific subclasses
+    // and then modifying the original items by reference.
     private val itemClassMap: Map<String, KFunction1<Item, TickableItem>> = mapOf(
         "Aged Brie" to ::AgedBrie,
         "Backstage passes to a TAFKAL80ETC concert" to ::BackstagePass,
@@ -13,14 +17,10 @@ class GildedRose(var items: List<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             val item = items[i]
-            tick(item)
+            val tickableItem = createTickableItem(item)
+            tickableItem.tick()
+            item.updateInPlace(tickableItem)
         }
-    }
-
-    private fun tick(item: Item) {
-        val tickableItem = createTickableItem(item)
-        tickableItem.tick()
-        item.updateUsing(tickableItem)
     }
 
     private fun createTickableItem(item: Item): TickableItem {
@@ -29,7 +29,7 @@ class GildedRose(var items: List<Item>) {
         return tickableItem
     }
 
-    fun Item.updateUsing(other: Item) {
+    fun Item.updateInPlace(other: TickableItem) {
         quality = other.quality;
         name = other.name;
         sellIn = other.sellIn
