3c1d92ae7fb12eb20d4b5aef4c8b41449591eb1f
Harm De Weirdt <harm@tripled.io>
2024-03-07 14:30:39 +0100

    simplify logic in AgedBrie

--- src/main/kotlin/com/gildedrose/AgedBrie.kt
+++ src/main/kotlin/com/gildedrose/AgedBrie.kt
@@ -1,16 +1,17 @@
 package com.gildedrose
 
+import kotlin.math.min
+
 class AgedBrie(item: Item) : TickableItem(item.name, item.sellIn, item.quality) {
 
     override fun tick() {
-        if (quality < 50) {
-            quality += 1
-        }
         sellIn -= 1
-        if (sellIn < 0) {
-            if (quality < 50) {
-                quality += 1
-            }
+        quality = if (isReallyAged()) {
+            min(quality + 2, 50)
+        } else {
+            min(quality + 1, 50)
         }
     }
+
+    private fun isReallyAged() = sellIn < 0
 }
