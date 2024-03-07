4202a4ab3ffe47ee8bbf96175fecf9fad8f01680
Harm De Weirdt <harm@tripled.io>
2024-03-07 14:21:50 +0100

    simplify logic in RegularItem

--- src/main/kotlin/com/gildedrose/RegularItem.kt
+++ src/main/kotlin/com/gildedrose/RegularItem.kt
@@ -1,16 +1,16 @@
 package com.gildedrose
 
+import kotlin.math.max
+
 class RegularItem(item: Item) : TickableItem(item.name, item.sellIn, item.quality) {
 
    override fun tick() {
-        if (quality > 0) {
-            quality -= 1
-        }
-        sellIn -= 1
-        if (sellIn < 0) {
-            if (quality > 0) {
-                quality -= 1
-            }
-        }
+       sellIn -= 1
+       if (isExpired()) {
+           quality = max(quality - 2, 0)
+       } else {
+           quality = max(quality - 1, 0)
+       }
     }
+
 }
--- src/main/kotlin/com/gildedrose/TickableItem.kt
+++ src/main/kotlin/com/gildedrose/TickableItem.kt
@@ -2,4 +2,5 @@ package com.gildedrose
 
 abstract class TickableItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality){
     abstract fun tick()
+    protected fun isExpired() = sellIn < 0
 }
